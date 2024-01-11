d41027f65e6a479d0ab227527a8bdb494d758a3d
PavanShyamendra <85981675+PavanShyamendra@users.noreply.github.com>
2024-01-11 10:17:32 +0530

    Changes the path to src/app

---
+++ src/app.js
@@ -0,0 +1,41 @@
+// Import necessary modules
+const express = require('express');
+const mongoose = require('mongoose');
+const serverless = require('serverless-http');
+
+
+// Create Express app
+const app = express();
+const router = express.Router();
+
+// MongoDB connection setup
+// mongoose.connect('mongodb://localhost:27017/my_database', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// const db = mongoose.connection;
+
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// db.once('open', () => {
+//   console.log('Connected to MongoDB');
+// });
+
+// Define routes or middleware here
+// For example:
+router.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.use('/.netlify/functions/api',router);
+
+const handler = serverless(app);
+
+module.exports.handler = async (event, context) => {
+  const result = await handler(event, context);
+  return result;
+};
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// })
